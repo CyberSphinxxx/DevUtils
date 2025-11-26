3bdc64380723fcb33749b1754de63162647aa134
CyberSphinxx <johnlemargonzales@gmail.com>
2025-11-26 20:50:43 +0800

    Add debounce function

---
+++ src/javascript/debounce.js
@@ -0,0 +1,16 @@
+/**
+ * Creates a debounced function that delays invoking func until after wait milliseconds have elapsed.
+ * @param {Function} func - The function to debounce.
+ * @param {number} wait - The number of milliseconds to delay.
+ * @returns {Function} - The debounced function.
+ */
+const debounce = (func, wait) => {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+};
+
+module.exports = debounce;
