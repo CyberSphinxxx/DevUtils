d6b14be9b7e6c597a36cfd912ee761ccfb580f54
CyberSphinxx <johnlemargonzales@gmail.com>
2025-11-29 01:12:28 +0800

    Add capitalize.js

---
+++ src/javascript/capitalize.js
@@ -0,0 +1,11 @@
+/**
+ * Capitalizes the first letter of a string.
+ * @param {string} str - The string to capitalize.
+ * @returns {string} The capitalized string.
+ */
+function capitalize(str) {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+module.exports = capitalize;
