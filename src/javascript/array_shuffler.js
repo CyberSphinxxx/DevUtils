3041f09fcabc1fa0814fdc6c227ed6c8782fd9eb
CyberSphinxx <johnlemargonzales@gmail.com>
2025-12-01 03:39:45 +0800

    Add array_shuffler.js utility

---
+++ src/javascript/array_shuffler.js
@@ -0,0 +1,24 @@
+/**
+ * Shuffles an array using the Fisher-Yates algorithm.
+ * @param {Array} array - The array to shuffle.
+ * @returns {Array} The shuffled array.
+ */
+function shuffleArray(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
+
+module.exports = shuffleArray;
