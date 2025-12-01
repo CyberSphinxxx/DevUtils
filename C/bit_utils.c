d994cc2f826fc6de77120b3d4bd57becf6d91929
CyberSphinxx <johnlemargonzales@gmail.com>
2025-12-02 04:31:30 +0800

    Refactor module_26.c to bit_utils.c

---
+++ C/bit_utils.c
@@ -0,0 +1,2 @@
+#define SET_BIT(n, k) ((n) |= (1 << (k)))
+#define CLR_BIT(n, k) ((n) &= ~(1 << (k)))
--- C/module_26.c
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-
-int* allocate_26() {
-    return (int*)malloc(sizeof(int) * 26);
-}
