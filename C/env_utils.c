ee5ff2e8cfe63e482baefdee4bf7bd545555e2c2
CyberSphinxx <johnlemargonzales@gmail.com>
2025-12-02 04:31:29 +0800

    Refactor module_24.c to env_utils.c

---
+++ C/env_utils.c
@@ -0,0 +1,2 @@
+#include <stdlib.h>
+char* get_env(const char* name) { return getenv(name); }
--- C/module_24.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello from file 24!\n");
-    return 0;
-}
